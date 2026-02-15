b1c767f6f3176462b4504cc4ebfc2785ceb0495f
sepidar13 <134430476+sepidar13@users.noreply.github.com>
2026-02-15 10:22:47 +0330

    Create rotateArray.js
    added a new codewar solution.

---
+++ 7kyu/sortedArray.js
@@ -0,0 +1,17 @@
+Description:
+Given any number of arrays each sorted in ascending order, find the nth smallest number of all their elements.
+
+All the arguments except the last will be arrays, the last argument is n.
+
+nthSmallest([1,5], [2], [4,8,9], 4) // returns 5 because it's the 4th smallest value
+Be mindful of performance.
+
+SOLUTION
+function nthSmallest(...args){
+  let arrays = [...args]
+  let n = arrays.pop()
+  let na = []
+  arrays.map(array => na.push(...array))
+  na.sort((a,b) => a - b)
+  return na[n-1]
+  }
