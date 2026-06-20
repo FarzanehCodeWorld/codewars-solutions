d8a40fde51b2931aa9380b7e7f70fdf741aa3225
Farzaneh Rabbani <134430476+FarzanehCodeWorld@users.noreply.github.com>
2026-06-20 08:16:32 +0330

    Create CHECK THESE LETTERS.js

---
+++ CHECK THESE LETTERS.js
@@ -0,0 +1,18 @@
+//DESCRIPTION
+// Write a function that checks if all the letters in the second string are present in the first one at least once, regardless of how many times they appear:
+
+// ["ab", "aaa"]    =>  true
+// ["trances", "nectar"]    =>  true
+// ["compadres", "DRAPES"]  =>  true
+// ["parses", "parsecs"]    =>  false
+// Function should not be case sensitive, as indicated in example #2. Note: both strings are presented as a single argument in the form of an array.
+
+//SOLUTION
+function letterCheck([dict,word]) {
+//write your code here!!
+  const s = new Set([...dict.toLowerCase()]);
+  console.log([...word.toLowerCase()])
+  return [...word.toLowerCase()].every(x => s.has(x));
+
+ 
+}
