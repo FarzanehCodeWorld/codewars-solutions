7df65aa0797ec96fbbdcc3e39c686ad66d794738
Farzaneh Rabbani <134430476+FarzanehCodeWorld@users.noreply.github.com>
2026-07-01 09:05:01 +0330

    Create pigLatin.js

---
+++ pigLatin.js
@@ -0,0 +1,33 @@
+// Description:
+
+// Pig Latin is an English language game where the goal is to hide the meaning of a word from people not aware of the rules.
+
+// So, the goal of this kata is to wite a function that encodes a single word string to pig latin.
+
+// The rules themselves are rather easy:
+
+// The word starts with a vowel(a,e,i,o,u) -> return the original string plus "way".
+
+// The word starts with a consonant -> move consonants from the beginning of the word to the end of the word until the first vowel, then return it plus "ay".
+
+// The result must be lowercase, regardless of the case of the input. If the input string has any non-alpha characters, the function must return None, null, Nothing (depending on the language).
+
+// The function must also handle simple random strings and not just English words.
+
+// The input string has no vowels -> return the original string plus "ay".
+
+// For example, the word "spaghetti" becomes "aghettispay" because the first two letters ("sp") are consonants, so they are moved to the end of the string and "ay" is appende
+
+//Solution:
+function pigLatingGame(string) {
+  string = string.toLowerCase();
+  if(string.search(/[^a-z]/) !== -1) return 'null';
+  let pos = string.search(/[aeuio]/);
+  //no vowels inside string
+  if(pos == -1) return string+'ay'
+  //first letter starts with vowel
+  if(pos == 0) return string+'way'
+  //starts with consonent
+  else(return string.slice(pos,string.length)+string.slice(0,pos)+'ay')
+  
+}
