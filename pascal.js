e6fc6d71fdf10544667b6064cf1536d4e4a8ea5d
Farzaneh Rabbani <134430476+FarzanehCodeWorld@users.noreply.github.com>
2026-07-23 08:35:26 +0330

    added pascal.js.

---
+++ pascal.js
@@ -0,0 +1,24 @@
+// Description:
+// Task
+// Write a function that, given a depth n, returns n top rows of Pascal's Triangle flattened into a one-dimensional list/array.
+
+// Example:
+// n = 1: [1]
+// n = 2: [1,  1, 1]
+// n = 4: [1,  1, 1,  1, 2, 1,  1, 3, 3, 1]
+// Note
+// Beware of overflow. Requested terms of a triangle are guaranteed to fit into the returned type, 
+//   but depending on selected method of calculations, intermediate values can be larger.
+
+function f(n) {
+  if(n<2) return 1;
+  return n * f(n - 1);
+}
+
+function pascalsTriangle(n) {
+ var r = [];
+ for(var i = 0; i < n; i++)
+   for(var e = 0; e <= i; e++)
+     r.push( Math.round(f(i)/(f(e)*f(i - e))));
+ return r;
+}
